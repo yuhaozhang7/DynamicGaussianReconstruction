fbe9824ef2a96e1e1f972d9a599a34977598ca7c
yuhaozhang7 <yuhao.zhang-7@outlook.com>
2024-12-06 11:00:08 +0000

    move warm up change to init file

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -79,7 +79,7 @@ class PipelineParams(ParamGroup):
 class OptimizationParams(ParamGroup):
     def __init__(self, parser):
         self.iterations = 400_000
-        self.warm_up = 5_000
+        self.warm_up = 1_000
         self.position_lr_init = 0.00016
         self.position_lr_final = 0.0000016
         self.position_lr_delay_mult = 0.01
